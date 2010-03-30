be9605cfdbf26bdd5912f8ece3391cef693782ec
Todd Kover <kovert@omniscient.com>
2010-03-30 23:51:05 +0000

    add scripts to populate approles.  its probably that this all will go away in favor of proper applications, but this is a good start.

---
+++ management/approle/apps-in.pl
@@ -0,0 +1,133 @@
+#!/usr/local/bin/perl
+# Copyright (c) 2005-2010, Vonage Holdings Corp.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY VONAGE HOLDINGS CORP. ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL VONAGE HOLDINGS CORP. BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# $Id$
+#
+
+#
+# Audits a sheet of a defined format against db
+#
+
+use strict;
+use warnings;
+use JazzHands::DBI;
+use Data::Dumper;
+use Carp;
+use FileHandle;
+
+my $in = "applications";
+
+exit do_work();
+
+sub lookfor {
+	my($dbh, $name) = @_;
+
+	my $findSth = $dbh->prepare_cached(qq{
+		select	device_collection_id
+		  from	device_collection
+		 where	name = :1
+		  and	device_collection_type = 'appgroup'
+	}) || die $dbh->errstr;
+
+	$findSth->execute($name) || die $findSth->errstr;
+	my ($id) = $findSth->fetchrow_array;
+	$findSth->finish;
+	$id;
+}
+
+sub do_work {
+	my $dbh = JazzHands::DBI->connect('apps', {AutoCommit=>0}) || confess;
+
+	{
+		my $dude = (getpwuid($<))[0] || 'unknown';
+		my $q = qq{ 
+			begin
+				dbms_session.set_identifier ('$dude');
+			end;
+		};
+		if(my $sth = $dbh->prepare($q)) {
+			$sth->execute || confess $sth->errstr;
+		}
+	}
+
+	my ($parentid, $lastindent) = (undef, 0);
+	my(@parentage);
+
+	my $fh = new FileHandle($in) || die "$in: $!";
+
+	my $DCsth = $dbh->prepare_cached(qq{
+		insert into device_collection 
+			(name, device_collection_type,SHOULD_GENERATE_SUDOERS)
+		values
+			(:name, 'appgroup', 'N')
+		returning device_collection_id into :dcid
+	}) || die $dbh->errstr;
+
+	my $HRsth = $dbh->prepare_cached(qq{
+		insert into device_collection_hier
+			(PARENT_DEVICE_COLLECTION_ID, DEVICE_COLLECTION_ID
+			)
+		values
+			(:1, :2)
+	}) || die $dbh->errstr;
+
+	while(my $line = $fh->getline) {
+		next if($line =~ /^\s*#/);
+		next if($line =~ /^\s*$/);
+		chomp($line);
+		next if($line =~ /^\s*$/);
+		my($indent, $name) = ($line =~ /^(\t+)-\s*(\S.+)\s*$/);
+
+		my $li = length($indent);
+
+
+		for(my $i = $lastindent; $i >= $li; $i--) {
+			pop(@parentage);
+		}
+		my $parent = $parentage[$#parentage] || "";
+
+		print "$#parentage $lastindent $li add $name, parent is ", (($parent)?$parent:"--none--"), "\n";
+		$lastindent = $li;
+
+		# if it already exists, skip, it means we're inserting new
+		# child records.  If new parentage is inserted this can cause
+		# dogs and cats to be living together.
+		my $id = lookfor($dbh, $name);
+		if(!$id) {
+			$DCsth->bind_param(':name', $name) || die $DCsth->errstr;
+			$DCsth->bind_param_inout(':dcid', \$id, 50) || die $DCsth->errstr;
+			$DCsth->execute || die $DCsth->errstr;
+			$DCsth->finish;
+
+			if($parent) {
+				$HRsth->execute($parent, $id) || die $HRsth->errstr;
+			}
+		}
+		push(@parentage, $id);
+		
+	}
+	$fh->close;
+
+	$dbh->commit;
+	$dbh->disconnect;
+}
