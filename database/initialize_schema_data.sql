1ea18fdbeecf39de88a3b58586258b8566f5c50f
Todd Kover <kovert@omniscient.com>
2011-02-03 04:39:48 +0000

    rename database/dml to database/init and split out oracle bits (scheduler)

--- database/initialize_schema_data.sql
+++ database/initialize_schema_data.sql
@@ -1,8 +1,8 @@
-@@dml/initialize_country_codes.sql
-@@dml/initialize_jazzhands.sql
-@@dml/initialize_jazzhands_optional.sql
+@@init/initialize_country_codes.sql
+@@init/initialize_jazzhands.sql
+@@init/initialize_jazzhands_optional.sql
 -- this is example data that should be inserted differently
--- @dml/initialize_jazzhands_example.sql
-@@dml/insert_blacklist.sql
+-- @init/initialize_jazzhands_example.sql
+@@init/insert_blacklist.sql
 -- XXX need to  sort out the permissions here
--- @@dml/submit_scheduler.sql
+-- @@init/oracle/submit_scheduler.sql
