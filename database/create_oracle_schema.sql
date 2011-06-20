c3010d193d0b42eafd00c667badd13f79bd9a81c
Todd Kover <kovert@omniscient.com>
2011-06-20 15:17:01 +0000

    fix Id keywords; move oracle schema creation to its own directory or consistency/prep for postgresql

--- database/create_oracle_schema.sql
+++ database/create_oracle_schema.sql
@@ -5,11 +5,11 @@ set echo on
 set termout on
 set serveroutput on
 select systimestamp from dual;
-@@ddl/schema/create_schema.sql
-@@ddl/schema/build_audit_tables.sql
+@@ddl/schema/oracle/create_schema.sql
+@@ddl/schema/oracle/build_audit_tables.sql
 -- (next item is generated above in the current directory)
 @@generated_audit_tables.sql
-@@ddl/schema/build_audit_triggers.sql
+@@ddl/schema/oracle/build_audit_triggers.sql
 -- (next item generated above into the current directory)
 @@generated_audit_triggers.sql
 @@pkg/oracle/create_early_packages.sql
@@ -17,8 +17,8 @@ select systimestamp from dual;
 @@ddl/views/oracle/create_oracle_views.sql
 @@ddl/views/create_extra_views_oracle.sql
 --
-@@ddl/schema/create_audit_indexes.sql
-@@ddl/schema/create_extra_objects.sql
+@@ddl/schema/oracle/create_audit_indexes.sql
+@@ddl/schema/oracle/create_extra_objects.sql
 @@pkg/oracle/create_all_packages.sql
-@@ddl/schema/create_triggers.sql
+@@ddl/schema/oracle/create_triggers.sql
 select systimestamp from dual;
