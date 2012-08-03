0efbaa34e1ff0200c0996d2f0e6d863822b0dacb
KZ Win <kwin@appnexus.com>
2012-08-03 20:00:41 +0000

    remove sensitive information in the error message

--- aaa/acct-mgmt/src/lib/lib/JazzHands/Krb5.pm
+++ aaa/acct-mgmt/src/lib/lib/JazzHands/Krb5.pm
@@ -12,7 +12,7 @@ our $VERSION = '0.01';
 sub admin {
         my $params = JazzHands::AppAuthAL::find_and_parse_auth($_[1],'','krb5');
         my $kadm = JazzHands::Krb5::Tools->new( %$params);
-	ref($kadm) or confess sprintf "Unable to get admin principal to change Kerberos password: %s", $kadm;
+	ref($kadm) or die sprintf "Unable to get admin principal to change Kerberos password";
 	$kadm
 }
 
