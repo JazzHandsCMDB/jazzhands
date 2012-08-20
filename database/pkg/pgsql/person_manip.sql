a2ba0ce7a4dd3b7b5b3be1a53907d78458ffee9b
Todd Kover <kovert@omniscient.com>
2012-08-20 22:25:52 +0000

    add purge_account and merge_account functions

--- database/pkg/pgsql/person_manip.sql
+++ database/pkg/pgsql/person_manip.sql
@@ -113,6 +113,7 @@ BEGIN
 	INSERT INTO account_collection_account (account_collection_id, account_id) VALUES ( _account_collection_id, account_id);
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
+
 CREATE OR REPLACE FUNCTION person_manip.add_account_non_person(_company_id integer, _account_status character varying, _login character varying, _description character varying) RETURNS integer
     LANGUAGE plpgsql SECURITY DEFINER
     AS $$
@@ -172,6 +173,116 @@ BEGIN
         RETURN new_id;
 END;
 
-$$
-        LANGUAGE plpgsql SECURITY DEFINER;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
 
+-- Purge account from system.  This is called rarely and does not hit
+-- a number of tables where account_id may appear.  The caller needs
+-- to deal with those manually because they are not properties of the
+-- account
+CREATE OR REPLACE FUNCTION person_manip.purge_account(
+		in_account_id	account.account_id%TYPE
+) RETURNS void AS $$
+BEGIN
+		DELETE FROM account_assignd_cert where ACCOUNT_ID = in_account_id;
+		DELETE FROM account_token where ACCOUNT_ID = in_account_id;
+		DELETE FROM account_unix_info where ACCOUNT_ID = in_account_id;
+		DELETE FROM klogin where ACCOUNT_ID = in_account_id;
+		DELETE FROM property where ACCOUNT_ID = in_account_id;
+		DELETE FROM account_password where ACCOUNT_ID = in_account_id;
+		DELETE FROM unix_group where account_collection_id in 
+			(select account_collection_id from account_collection where account_collection_name in
+				(select login from account where account_id = in_account_id)
+				and account_collection_type in ('unix-group')
+			);
+		DELETE FROM account_collection_account where ACCOUNT_ID = in_account_id;
+
+		DELETE FROM account_collection where account_collection_name in 
+			(select login from account where account_id = in_account_id)
+			and account_collection_type in ('per-user', 'unix-group');
+
+		DELETE FROM account where ACCOUNT_ID = in_account_id;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+CREATE OR REPLACE FUNCTION person_manip.merge_accounts(
+	merge_from_account_id	account.account_Id%TYPE,
+	merge_to_account_id	account.account_Id%TYPE
+) RETURNS INTEGER AS $$
+DECLARE
+	fpc		person_company%ROWTYPE;
+	tpc		person_company%ROWTYPE;
+BEGIN
+	select	*
+	  into	fpc
+	  from	person_company
+	 where	(person_id, company_id) in
+		(select person_id, company_id 
+		   from account where account_id = merge_from_account_id);
+
+	select	*
+	  into	tpc
+	  from	person_company
+	 where	(person_id, company_id) in
+		(select person_id, company_id 
+		   from account where account_id = merge_to_account_id);
+
+	IF (fpc.company_id != tpc.company_id) THEN
+		RAISE EXCEPTION 'Accounts are in different companies';
+	END IF;
+
+	IF (fpc.person_company_relation != tpc.person_company_relation) THEN
+		RAISE EXCEPTION 'People have different relationships';
+	END IF;
+
+	IF(tpc.external_hr_id is NOT NULL) THEN
+		RAISE EXCEPTION 'Destination account has an external HR ID';
+	END IF;
+
+	-- Switch the HR ID
+	UPDATE PERSON_COMPANY
+	  SET  EXTERNAL_HR_ID = NULL
+	WHERE	PERSON_ID = fpc.person_id
+	  AND	COMPANY_ID = fpc.company_id;
+
+	UPDATE PERSON_COMPANY
+	  SET  EXTERNAL_HR_ID = fpc.external_hr_id
+	WHERE	PERSON_ID = tpc.person_id
+	  AND	COMPANY_ID = tpc.company_id;
+
+	-- move any account collections over that are
+	-- not infrastructure ones, and the new person is
+	-- not in
+	UPDATE	account_collection_account
+	   SET	ACCOUNT_ID = merge_to_account_id
+	 WHERE	ACCOUNT_ID = merge_from_account_id
+	  AND	ACCOUNT_COLLECTION_ID NOT IN (
+			SELECT ACCOUNT_COLLECTION_ID
+			  FROM	ACCOUNT_COLLECTION
+				INNER JOIN VAL_ACCOUNT_COLLECTION_TYPE
+					USING (ACCOUNT_COLLECTION_TYPE)
+			 WHERE	IS_INFRASTRUCTURE_TYPE = 'N'
+		)
+	  AND	account_collection_id not in (
+			SELECT	account_collection_id
+			  FROM	account_collection_account
+			 WHERE	account_id != merge_to_account_id
+	);
+
+	-- Now begin removing the old account
+	PERFORM person_manip.purge_account( merge_from_account_id );
+
+	update person_contact set person_id = tpc.person_id where person_id = fpc.person_id;
+	update person_image set person_id = tpc.person_id where person_id = fpc.person_id;
+	update person_vehicle set person_id = tpc.person_id where person_id = fpc.person_id;
+	update property set person_id = tpc.person_id where person_id = fpc.person_id;
+	delete from person_account_realm_company where person_id = fpc.person_id AND company_id = fpc.company_id;
+	delete from person_company where person_id = fpc.person_id AND company_id = fpc.company_id;
+	-- if there are other relations that may exist, do not delete the person.
+	BEGIN
+		delete from person where person_id = fpc.person_id;
+	EXCEPTION WHEN foreign_key_violation THEN
+		NULL;
+	END;
+	return merge_to_account_id;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
