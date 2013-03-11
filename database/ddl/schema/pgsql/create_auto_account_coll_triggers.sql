0280bf1e3983eab942a70b0983e50a77aed3d250
KZWin <kwin@appnexus.com>
2013-03-11 16:46:39 -0400

    support two more automated account collections based on is_mangement and is_full_time columns in person_company and gender column in person triggers on person_company and person to manage these ACs where these columns change emit warnings when 	account_id, company_id, account_realm_id changes on account table 	person_id, company_id changes on person_company table 	person_id changes on person table

--- database/ddl/schema/pgsql/create_auto_account_coll_triggers.sql
+++ database/ddl/schema/pgsql/create_auto_account_coll_triggers.sql
@@ -4,11 +4,12 @@ DECLARE
 	c_name VARCHAR;
 	sc VARCHAR;
 	ac_ids INTEGER[];
-	_exempt VARCHAR;
-	exempt_status VARCHAR;
 	delete_aca BOOLEAN;
 	_gender VARCHAR;
+	_person_company RECORD;
+	acr_c_name VARCHAR;
 	gender_string VARCHAR;
+	_status RECORD;
 BEGIN
 	IF TG_OP = 'INSERT' THEN
 		IF NEW.account_role != 'primary' THEN
@@ -18,43 +19,68 @@ BEGIN
 		IF NOT FOUND THEN
 			RETURN NEW;
 		END IF;
+	-- The triggers need not deal with account realms companies or sites being renamed, although we may want to revisit this later.
+	ELSIF NEW.account_id != OLD.account_id OR NEW.company_id != OLD.company_id OR NEW.account_realm_id != OLD.account_realm_id THEN
+		RAISE NOTICE 'This trigger does not handle changing account_realm_id or company_id or account id';
+		RETURN NEW;
 	END IF;
-	ac_ids = '{-1,-1,-1,-1,-1}';
+	ac_ids = '{-1,-1,-1,-1,-1,-1,-1}';
 	SELECT account_realm_name INTO acr FROM account_realm WHERE account_realm_id = NEW.account_realm_id;
 	ac_ids[0] = acct_coll_manip.get_automated_account_collection_id(acr || '_' || NEW.account_type);
 	SELECT company_short_name INTO c_name FROM company WHERE company_id = NEW.company_id AND company_short_name IS NOT NULL;
 	IF NOT FOUND THEN
 		RAISE NOTICE 'Company short name cannot be determined from company_id % in %', NEW.company_id, TG_NAME;
 	ELSE
-		ac_ids[1] = acct_coll_manip.get_automated_account_collection_id(acr || '_' || c_name || '_' || NEW.account_type);
-		SELECT is_exempt INTO _exempt FROM person_company JOIN account a USING (person_id) WHERE a.person_id != 0 AND account_id = NEW.account_id AND is_exempt IS NOT NULL;
+		acr_c_name = acr || '_' || c_name;
+		ac_ids[1] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || NEW.account_type);
+		SELECT
+			pc.*
+		INTO
+			_person_company
+		FROM
+			person_company pc
+		JOIN
+			account a
+		USING
+			(person_id)
+		WHERE
+			a.person_id != 0 AND account_id = NEW.account_id;
 		IF FOUND THEN
-			exempt_status = 'exempt';
-			IF _exempt = 'N' THEN
-				exempt_status = 'nonexempt';
+			IF _person_company.is_exempt IS NOT NULL THEN
+				SELECT * INTO _status FROM acct_coll_manip.person_company_flags_to_automated_ac_name(_person_company.is_exempt, 'exempt');
+				ac_ids[2] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || _status.old);
 			END IF;
-			ac_ids[3] = acct_coll_manip.get_automated_account_collection_id(acr || '_' || c_name || '_' || exempt_status);
+			IF _person_company.is_full_time IS NOT NULL THEN
+				SELECT * INTO _status FROM acct_coll_manip.person_company_flags_to_automated_ac_name(_person_company.is_full_time, 'full_time');
+				ac_ids[3] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || _status.old);
+			END IF;
+			IF _person_company.is_management IS NOT NULL THEN
+				SELECT * INTO _status FROM acct_coll_manip.person_company_flags_to_automated_ac_name(_person_company.is_management, 'management');
+				ac_ids[4] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || _status.old);
+			END IF;
 		END IF;
-		SELECT gender INTO _gender FROM person JOIN account a USING (person_id) WHERE account_id = NEW.account_id AND a.person_id !=0 AND gender IS NOT NULL;
+		SELECT
+			gender
+		INTO
+			_gender
+		FROM
+			person
+		JOIN
+			account a
+		USING
+			(person_id)
+		WHERE
+			account_id = NEW.account_id AND a.person_id !=0 AND gender IS NOT NULL;
 		IF FOUND THEN
-			gender_string = '';
-			IF _gender = 'M' THEN
-				gender_string = 'male';
-			ELSIF _gender = 'F' THEN
-				gender_string = 'female';
-			ELSIF _gender = 'U' THEN
-				gender_string = 'unspecified';
-			END IF;
-			IF gender_string = '' THEN
-				RAISE NOTICE 'Gender string not determined';
-			ELSE
-				ac_ids[4] = acct_coll_manip.get_automated_account_collection_id(acr || '_' || c_name || '_' || gender_string);
+			gender_string = acct_coll_manip.person_gender_flag_to_automated_ac_name(_gender);
+			IF gender_string IS NOT NULL THEN
+				ac_ids[5] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || gender_string);
 			END IF;
 		END IF;
 	END IF;
 	SELECT site_code INTO sc FROM person_location WHERE person_id = NEW.person_id AND site_code IS NOT NULL;
 	IF FOUND THEN
-		ac_ids[2] = acct_coll_manip.get_automated_account_collection_id(acr || '_' || sc);
+		ac_ids[6] = acct_coll_manip.get_automated_account_collection_id(acr || '_' || sc);
 	END IF;
 	delete_aca = 't';
 	IF TG_OP = 'INSERT' THEN
@@ -83,6 +109,191 @@ $_$ LANGUAGE plpgsql SECURITY DEFINER;
 DROP TRIGGER IF EXISTS trig_automated_ac ON account;
 CREATE TRIGGER trig_automated_ac AFTER INSERT OR UPDATE ON account FOR EACH ROW EXECUTE PROCEDURE automated_ac();
 
+CREATE OR REPLACE FUNCTION automated_ac_on_person_company() RETURNS TRIGGER AS $_$
+DECLARE
+	ac_id INTEGER[];
+	c_name VARCHAR;
+	old_c_name VARCHAR;
+	old_acr_c_name VARCHAR;
+	acr_c_name VARCHAR;
+	exempt_status VARCHAR;
+	full_time_status VARCHAR;
+	manager_status VARCHAR;
+	old_r RECORD;
+	r RECORD;
+	_status RECORD;
+BEGIN
+	-- at this time person_company.is_exempt column can be null.
+	-- take into account of is_exempt going from null to not null
+	IF NEW.is_exempt IS NOT NULL AND OLD.is_exempt IS NOT NULL AND NEW.is_exempt = OLD.is_exempt AND NEW.is_management = OLD.is_management AND NEW.is_full_time = OLD.is_full_time OR NEW.person_id = 0 AND OLD.person_id = 0 THEN
+		RETURN NEW;
+	END IF;
+	IF NEW.person_id != OLD.person_id OR NEW.company_id != OLD.company_id THEN
+		RAISE NOTICE 'This trigger % does not support changing person_id or company_id', TG_NAME;
+		RETURN NEW;
+	END IF;
+	SELECT company_short_name INTO old_c_name FROM company WHERE company_id = OLD.company_id AND company_short_name IS NOT NULL;
+	IF NOT FOUND THEN
+		RAISE NOTICE 'Company short name cannot be determined from company_id % in trigger %', OLD.company_id, TG_NAME;
+	END IF;
+	c_name = old_c_name;
+	FOR old_r
+		IN SELECT
+			account_realm_name, account_id
+		FROM
+			account_realm ar
+		JOIN
+			account a
+		USING
+			(account_realm_id)
+		JOIN
+			val_person_status vps
+		ON
+			account_status = vps.person_status AND vps.is_disabled='N'
+		WHERE
+			a.person_id = OLD.person_id AND a.company_id = OLD.company_id 
+	LOOP
+		old_acr_c_name = old_r.account_realm_name || '_' || old_c_name;
+		FOR r
+			IN SELECT
+				account_realm_name, account_id
+			FROM
+				account_realm ar
+			JOIN
+				account a
+			USING
+				(account_realm_id)
+			JOIN
+				val_person_status vps
+			ON
+				account_status = vps.person_status AND vps.is_disabled='N'
+			WHERE
+				a.person_id = NEW.person_id AND a.company_id = NEW.company_id
+		LOOP
+			acr_c_name = r.account_realm_name || '_' || c_name;
+			IF coalesce(NEW.is_exempt,'') != coalesce(OLD.is_exempt,'') THEN
+				SELECT * INTO _status FROM acct_coll_manip.person_company_flags_to_automated_ac_name(OLD.is_exempt, 'exempt');
+				IF old_acr_c_name IS NOT NULL THEN
+					DELETE FROM account_collection_account WHERE account_id = old_r.account_id
+						AND account_collection_id = acct_coll_manip.get_automated_account_collection_id(old_acr_c_name || '_' || _status.old); 
+				END IF;
+				IF acr_c_name IS NOT NULL THEN
+					ac_id[0] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || _status.new);
+					PERFORM acct_coll_manip.insert_or_delete_automated_ac('f', r.account_id, ac_id);
+				END IF;
+			END IF;
+			IF NEW.is_full_time != OLD.is_full_time THEN
+				SELECT * INTO _status FROM acct_coll_manip.person_company_flags_to_automated_ac_name(OLD.is_full_time, 'full_time');
+				IF old_acr_c_name IS NOT NULL THEN
+					DELETE FROM account_collection_account WHERE account_id = old_r.account_id
+						AND account_collection_id = acct_coll_manip.get_automated_account_collection_id(old_acr_c_name || '_' || _status.old);
+				END IF;
+				IF acr_c_name IS NOT NULL THEN
+					ac_id[0] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || _status.new);
+					PERFORM acct_coll_manip.insert_or_delete_automated_ac('f', r.account_id, ac_id);
+				END IF;
+			END IF;
+			IF NEW.is_management != OLD.is_management THEN
+				SELECT * INTO _status FROM acct_coll_manip.person_company_flags_to_automated_ac_name(OLD.is_management, 'management');
+				IF old_acr_c_name IS NOT NULL THEN
+					DELETE FROM account_collection_account WHERE account_id = old_r.account_id
+						AND account_collection_id = acct_coll_manip.get_automated_account_collection_id(old_acr_c_name || '_' || _status.old);
+				END IF;
+				IF acr_c_name IS NOT NULL THEN
+					ac_id[0] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || _status.new);
+					PERFORM acct_coll_manip.insert_or_delete_automated_ac('f', r.account_id, ac_id);
+				END IF;
+			END IF;
+		END LOOP;
+	END LOOP;
+	RETURN NEW;
+END;
+$_$ LANGUAGE plpgsql SECURITY DEFINER;
+DROP TRIGGER IF EXISTS trig_automated_ac ON person_company;
+CREATE TRIGGER trig_automated_ac AFTER UPDATE ON person_company FOR EACH ROW EXECUTE PROCEDURE automated_ac_on_person_company();
+
+CREATE OR REPLACE FUNCTION automated_ac_on_person() RETURNS TRIGGER AS $_$
+DECLARE
+	ac_id INTEGER[];
+	c_name VARCHAR;
+	old_c_name VARCHAR;
+	old_acr_c_name VARCHAR;
+	acr_c_name VARCHAR;
+	gender_string VARCHAR;
+	r RECORD;
+	old_r RECORD;
+BEGIN
+	IF NEW.gender = OLD.gender OR NEW.person_id = 0 AND OLD.person_id = 0 THEN
+		RETURN NEW;
+	END IF;
+	IF OLD.person_id ! = NEW.person_id THEN
+		RAISE NOTICE 'This trigger % does not support changing person_id', TG_NAME;
+		RETURN NEW;
+	END IF;
+	FOR old_r
+		IN SELECT
+			account_realm_name, account_id, company_id
+		FROM
+			account_realm ar
+		JOIN
+			account a
+		USING
+			(account_realm_id)
+		JOIN
+			val_person_status vps
+		ON
+			account_status = vps.person_status AND vps.is_disabled='N'
+		WHERE
+			a.person_id = OLD.person_id
+	LOOP
+		SELECT company_short_name INTO old_c_name FROM company WHERE company_id = old_r.company_id AND company_short_name IS NOT NULL;
+		IF FOUND THEN
+			old_acr_c_name = old_r.account_realm_name || '_' || old_c_name;
+			gender_string = acct_coll_manip.person_gender_flag_to_automated_ac_name(OLD.gender);
+			IF gender_string IS NOT NULL THEN
+				DELETE FROM account_collection_account WHERE account_id = old_r.account_id
+					AND account_collection_id = acct_coll_manip.get_automated_account_collection_id(old_acr_c_name || '_' ||  gender_string);
+			END IF;
+		ELSE
+			RAISE NOTICE 'Company short name cannot be determined from company_id % in %', old_r.company_id, TG_NAME;
+		END IF;
+		FOR r
+			IN SELECT
+				account_realm_name, account_id, company_id
+			FROM
+				account_realm ar
+			JOIN
+				account a
+			USING
+				(account_realm_id)
+			JOIN
+				val_person_status vps
+			ON
+				account_status = vps.person_status AND vps.is_disabled='N'
+			WHERE
+				a.person_id = NEW.person_id
+		LOOP
+			-- do not support changing company per specification in the email
+			c_name = old_c_name;
+			IF c_name IS NULL THEN
+				RAISE NOTICE 'Company short name cannot be determined from company_id % in %', NEW.company_id, TG_NAME;
+				CONTINUE;
+			END IF;
+			acr_c_name = r.account_realm_name || '_' || c_name;
+			gender_string = acct_coll_manip.person_gender_flag_to_automated_ac_name(NEW.gender);
+			IF gender_string IS NULL THEN
+				CONTINUE;
+			END IF;
+			ac_id[0] = acct_coll_manip.get_automated_account_collection_id(acr_c_name || '_' || gender_string);
+			PERFORM acct_coll_manip.insert_or_delete_automated_ac('f', r.account_id, ac_id);
+		END LOOP;
+	END LOOP;
+	RETURN NEW;
+END;
+$_$ LANGUAGE plpgsql SECURITY DEFINER;
+DROP TRIGGER IF EXISTS trig_automated_ac ON person;
+CREATE TRIGGER trig_automated_ac AFTER UPDATE ON person FOR EACH ROW EXECUTE PROCEDURE automated_ac_on_person();
+
 CREATE OR REPLACE FUNCTION automated_realm_site_ac_pl() RETURNS TRIGGER AS $_$
 DECLARE
 	sc VARCHAR;
@@ -110,10 +321,21 @@ BEGIN
 		sc = NEW.site_code;
 	END IF;
 
-	FOR r IN SELECT account_realm_name, account_id FROM account_realm ar
-		JOIN account a ON ar.account_realm_id=a.account_realm_id AND a.account_role = 'primary' AND a.person_id = p_id 
-		JOIN val_person_status vps ON vps.person_status = a.account_status AND vps.is_disabled='N'
-		JOIN site s ON s.site_code = sc AND a.company_id = s.colo_company_id
+	FOR r IN SELECT account_realm_name, account_id
+		FROM
+			account_realm ar
+		JOIN
+			account a
+		ON
+			ar.account_realm_id=a.account_realm_id AND a.account_role = 'primary' AND a.person_id = p_id 
+		JOIN
+			val_person_status vps
+		ON
+			vps.person_status = a.account_status AND vps.is_disabled='N'
+		JOIN
+			site s
+		ON
+			s.site_code = sc AND a.company_id = s.colo_company_id
 	LOOP
 		IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
 			ac_name = r.account_realm_name || '_' || sc;
