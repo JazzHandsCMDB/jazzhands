59eb6ef83b9d1185e021d608cb8b3bc346a0b204
Bernard Jech <bjech@appnexus.com>
2013-07-19 10:10:45 -0400

    Added schema parameters to audit trigger building functions

--- database/ddl/schema/pgsql/create_schema_support.sql
+++ database/ddl/schema/pgsql/create_schema_support.sql
@@ -3,8 +3,8 @@
 -- $Id$
 --
 
-drop schema IF EXISTS schema_support;
-create schema schema_support authorization jazzhands;
+-- DROP SCHEMA IF EXISTS schema_support;
+-- CREATE SCHEMA schema_support AUTHORIZATION jazzhands;
 
 -------------------------------------------------------------------
 -- returns the Id tag for CM
@@ -12,212 +12,223 @@ create schema schema_support authorization jazzhands;
 CREATE OR REPLACE FUNCTION schema_support.id_tag()
 RETURNS VARCHAR AS $$
 BEGIN
-        RETURN('<-- $Id -->');
+    RETURN('<-- $Id -->');
 END;
 $$ LANGUAGE plpgsql;
 -- end of procedure id_tag
 -------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION schema_support.rebuild_audit_trigger(name varchar)
+CREATE OR REPLACE FUNCTION schema_support.rebuild_audit_trigger
+    ( aud_schema VARCHAR, tbl_schema VARCHAR, table_name VARCHAR )
 RETURNS VOID AS $$
-DECLARE
-	create_text	VARCHAR;
 BEGIN
-	EXECUTE 'CREATE OR REPLACE FUNCTION ' || quote_ident('perform_audit_' || 
-			name) || $ZZ$() RETURNS TRIGGER AS $TQ$
-		DECLARE
-			appuser VARCHAR;
-		BEGIN
-			BEGIN
-				appuser := session_user || '/' || current_setting('jazzhands.appuser');
-			EXCEPTION
-				WHEN OTHERS THEN
-					appuser := session_user;
-			END;
+    EXECUTE 'CREATE OR REPLACE FUNCTION ' || quote_ident(tbl_schema)
+        || '.' || quote_ident('perform_audit_' || table_name)
+        || $ZZ$() RETURNS TRIGGER AS $TQ$
+            DECLARE
+                appuser VARCHAR;
+            BEGIN
+                BEGIN
+                    appuser := session_user
+                        || '/' || current_setting('jazzhands.appuser');
+                EXCEPTION WHEN OTHERS THEN
+                    appuser := session_user;
+                END;
 
-			IF TG_OP = 'DELETE' THEN
-				INSERT INTO audit.$ZZ$ || quote_ident(name) || $ZZ$ VALUES (
-					OLD.*, 'DEL', now(), appuser);
-				RETURN OLD;
-			ELSIF TG_OP = 'UPDATE' THEN
-				INSERT INTO audit.$ZZ$ || quote_ident(name) || $ZZ$ VALUES (
-					NEW.*, 'UPD', now(), appuser);
-				RETURN NEW;
-			ELSIF TG_OP = 'INSERT' THEN
-				INSERT INTO audit.$ZZ$ || quote_ident(name) || $ZZ$ VALUES (
-					NEW.*, 'INS', now(), appuser);
-				RETURN NEW;
-			END IF;
-			RETURN NULL;
-		END;
-		$TQ$ LANGUAGE plpgsql SECURITY DEFINER
-	$ZZ$;
-	EXECUTE 'DROP TRIGGER IF EXISTS ' ||
-		quote_ident('trigger_audit_' || name) || ' ON ' || quote_ident(name);
-	EXECUTE 'CREATE TRIGGER ' ||
-		quote_ident('trigger_audit_' || name) || 
-			' AFTER INSERT OR UPDATE OR DELETE ON ' ||
-			quote_ident(name) || ' FOR EACH ROW EXECUTE PROCEDURE ' ||
-			quote_ident('perform_audit_' || name) || '()';
+                IF TG_OP = 'DELETE' THEN
+                    INSERT INTO $ZZ$ || quote_ident(aud_schema) 
+                        || '.' || quote_ident(table_name) || $ZZ$
+                    VALUES ( OLD.*, 'DEL', now(), appuser );
+                    RETURN OLD;
+                ELSIF TG_OP = 'UPDATE' THEN
+                    INSERT INTO $ZZ$ || quote_ident(aud_schema)
+                        || '.' || quote_ident(table_name) || $ZZ$
+                    VALUES ( NEW.*, 'UPD', now(), appuser );
+                    RETURN NEW;
+                ELSIF TG_OP = 'INSERT' THEN
+                    INSERT INTO $ZZ$ || quote_ident(aud_schema)
+                        || '.' || quote_ident(table_name) || $ZZ$
+                    VALUES ( NEW.*, 'INS', now(), appuser );
+                    RETURN NEW;
+                END IF;
+                RETURN NULL;
+            END;
+        $TQ$ LANGUAGE plpgsql SECURITY DEFINER
+    $ZZ$;
+
+    EXECUTE 'DROP TRIGGER IF EXISTS ' || quote_ident('trigger_audit_'
+        || table_name) || ' ON ' || quote_ident(tbl_schema) || '.'
+        || quote_ident(table_name);
+
+    EXECUTE 'CREATE TRIGGER ' || quote_ident('trigger_audit_' || table_name)
+        || ' AFTER INSERT OR UPDATE OR DELETE ON ' || quote_ident(tbl_schema)
+        || '.' || quote_ident(table_name) || ' FOR EACH ROW EXECUTE PROCEDURE ' 
+        || quote_ident(tbl_schema) || '.' || quote_ident('perform_audit_'
+        || table_name) || '()';
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION schema_support.rebuild_audit_triggers()
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION schema_support.rebuild_audit_triggers
+    ( aud_schema varchar, tbl_schema varchar )
 RETURNS VOID AS $$
 DECLARE
-	table_list	RECORD;
-	name		VARCHAR;
+    table_list RECORD;
 BEGIN
-	--
-	-- select tables with audit tables
-	--
-	FOR table_list IN SELECT table_name FROM information_schema.tables
-		WHERE
-			table_type = 'BASE TABLE' AND
-			table_schema = 'jazzhands' AND
-			table_name IN (
-				SELECT
-					table_name 
-				FROM
-					information_schema.tables
-				WHERE
-					table_schema = 'audit' AND
-					table_type = 'BASE TABLE'
-				)
-		ORDER BY
-			table_name
-	LOOP
-		name := table_list.table_name;
-		PERFORM schema_support.rebuild_audit_trigger(name);
-	END LOOP;
+    --
+    -- select tables with audit tables
+    --
+    FOR table_list IN
+        SELECT table_name FROM information_schema.tables
+        WHERE table_type = 'BASE TABLE' AND table_schema = tbl_schema
+        AND table_name IN (
+            SELECT table_name FROM information_schema.tables
+            WHERE table_schema = aud_schema AND table_type = 'BASE TABLE'
+        ) ORDER BY table_name
+    LOOP
+        PERFORM schema_support.rebuild_audit_trigger
+            (aud_schema, tbl_schema, table_list.table_name);
+    END LOOP;
 END;
 $$ LANGUAGE plpgsql;
+
+-------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION schema_support.build_audit_table(
-	name varchar,
-	first_time boolean DEFAULT true
-) RETURNS VOID AS $FUNC$
-DECLARE
-	create_text	VARCHAR;
+    aud_schema VARCHAR, tbl_schema VARCHAR, table_name VARCHAR,
+    first_time boolean DEFAULT true
+)
+RETURNS VOID AS $FUNC$
 BEGIN
-	if first_time = true THEN
-		EXECUTE 'CREATE SEQUENCE audit.' || quote_ident(name || '_seq');
-	end if;
-	EXECUTE 'CREATE TABLE audit.' || quote_ident(name) || ' AS
-		SELECT *,
-			NULL::char(3) as "aud#action",
-			now() as "aud#timestamp",
-			NULL::varchar(30) AS "aud#user",
-			NULL::integer AS "aud#seq"
-		FROM ' || quote_ident(name) || ' LIMIT 0';
-	EXECUTE 'ALTER TABLE audit.' || quote_ident(name ) ||
-		$$ ALTER COLUMN "aud#seq" SET NOT NULL,
-		ALTER COLUMN "aud#seq" SET DEFAULT nextval('audit.$$ ||
-			name || '_seq' || $$')$$;
-	IF first_time = true THEN
-		PERFORM schema_support.rebuild_audit_trigger(name);
-	END IF;
+    IF first_time THEN
+        EXECUTE 'CREATE SEQUENCE ' || quote_ident(aud_schema) || '.'
+            || quote_ident(table_name || '_seq');
+    END IF;
+
+    EXECUTE 'CREATE TABLE ' || quote_ident(aud_schema) || '.'
+        || quote_ident(table_name) || ' AS '
+        || 'SELECT *, NULL::char(3) as "aud#action", now() as "aud#timestamp", '
+        || 'NULL::varchar(30) AS "aud#user", NULL::integer AS "aud#seq" '
+        || 'FROM ' || quote_ident(tbl_schema) || '.' || quote_ident(table_name) 
+        || ' LIMIT 0';
+
+    EXECUTE 'ALTER TABLE ' || quote_ident(aud_schema) || '.'
+        || quote_ident(table_name)
+        || $$ ALTER COLUMN "aud#seq" SET NOT NULL, $$
+        || $$ ALTER COLUMN "aud#seq" SET DEFAULT nextval('$$
+        || quote_ident(aud_schema) || '.' || quote_ident(table_name || '_seq')
+        || $$')$$;
+
+    IF first_time THEN
+        PERFORM schema_support.rebuild_audit_trigger
+            ( aud_schema, tbl_schema, table_name );
+    END IF;
 END;
 $FUNC$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION schema_support.build_audit_tables()
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION schema_support.build_audit_tables
+    ( aud_schema varchar, tbl_schema varchar )
 RETURNS VOID AS $FUNC$
 DECLARE
-	table_list	RECORD;
-	create_text	VARCHAR;
-	name		VARCHAR;
+     table_list RECORD;
 BEGIN
-	FOR table_list IN SELECT table_name FROM information_schema.tables
-		WHERE
-			table_type = 'BASE TABLE' AND
-			table_schema = 'jazzhands' AND
-			NOT( table_name IN (
-				SELECT
-					table_name 
-				FROM
-					information_schema.tables
-				WHERE
-					table_schema = 'audit'
-				)
-			)
-		ORDER BY
-			table_name
-	LOOP
-		name := table_list.table_name;
-		PERFORM schema_support.build_audit_table(name);
-	END LOOP;
-	PERFORM schema_support.rebuild_audit_triggers();
+    FOR table_list IN
+        SELECT table_name FROM information_schema.tables
+        WHERE table_type = 'BASE TABLE' AND table_schema = tbl_schema
+        AND NOT ( 
+            table_name IN (
+                SELECT table_name FROM information_schema.tables
+                WHERE table_schema = aud_schema
+            )
+        )
+        ORDER BY table_name
+    LOOP
+        PERFORM schema_support.build_audit_table
+            ( aud_schema, tbl_schema, table_list.table_name );
+    END LOOP;
+
+    PERFORM schema_support.rebuild_audit_triggers();
 END;
 $FUNC$ LANGUAGE plpgsql;
+
+-------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION schema_support.trigger_ins_upd_generic_func()
 RETURNS TRIGGER AS $$
 DECLARE
-	appuser	VARCHAR;
+    appuser VARCHAR;
 BEGIN
-	BEGIN
-		appuser := session_user || '/' || current_setting('jazzhands.appuser');
-	EXCEPTION
-		WHEN OTHERS THEN
-			appuser := session_user;
-	END;
-	IF TG_OP = 'INSERT' THEN
-		NEW.data_ins_user = appuser;
-		NEW.data_ins_date = 'now';
-	END IF;
+    BEGIN
+        appuser := session_user || '/' || current_setting('jazzhands.appuser');
+    EXCEPTION
+        WHEN OTHERS THEN appuser := session_user;
+    END;
 
-	if TG_OP = 'UPDATE' THEN
-		NEW.data_upd_user = appuser;
-		NEW.data_upd_date = 'now';
-		IF OLD.data_ins_user != NEW.data_ins_user then
-			RAISE EXCEPTION
-				'Non modifiable column "DATA_INS_USER" cannot be modified.';
-		END IF;
-		IF OLD.data_ins_date != NEW.data_ins_date then
-			RAISE EXCEPTION
-				'Non modifiable column "DATA_INS_DATE" cannot be modified.';
-		END IF;
-	END IF;
-	RETURN NEW;
+    IF TG_OP = 'INSERT' THEN
+        NEW.data_ins_user = appuser;
+        NEW.data_ins_date = 'now';
+    END IF;
+
+    IF TG_OP = 'UPDATE' THEN
+        NEW.data_upd_user = appuser;
+        NEW.data_upd_date = 'now';
+
+        IF OLD.data_ins_user != NEW.data_ins_user THEN
+            RAISE EXCEPTION
+                'Non modifiable column "DATA_INS_USER" cannot be modified.';
+        END IF;
+
+        IF OLD.data_ins_date != NEW.data_ins_date THEN
+            RAISE EXCEPTION
+                'Non modifiable column "DATA_INS_DATE" cannot be modified.';
+        END IF;
+    END IF;
+
+    RETURN NEW;
+
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
-CREATE OR REPLACE FUNCTION schema_support.rebuild_stamp_trigger(name varchar)
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION schema_support.rebuild_stamp_trigger
+    (tbl_schema VARCHAR, table_name VARCHAR)
 RETURNS VOID AS $$
 BEGIN
-	BEGIN
-		EXECUTE 'DROP TRIGGER IF EXISTS ' ||
-			quote_ident('trig_userlog_' || name) ||
-			' ON ' || quote_ident(name);
-		EXECUTE 'CREATE TRIGGER ' ||
-			quote_ident('trig_userlog_' || name) ||
-			' BEFORE INSERT OR UPDATE ON ' ||
-			quote_ident(name) ||
-			' FOR EACH ROW EXECUTE PROCEDURE 
-			schema_support.trigger_ins_upd_generic_func()';
-	END;
+    EXECUTE 'DROP TRIGGER IF EXISTS '
+        || quote_ident('trig_userlog_' || table_name)
+        || ' ON ' || quote_ident(tbl_schema) || '.' || quote_ident(table_name);
+
+    EXECUTE 'CREATE TRIGGER '
+        || quote_ident('trig_userlog_' || table_name)
+        || ' BEFORE INSERT OR UPDATE ON '
+        || quote_ident(tbl_schema) || '.' || quote_ident(table_name)
+        || ' FOR EACH ROW EXECUTE PROCEDURE'
+        || ' schema_support.trigger_ins_upd_generic_func()';
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
-CREATE OR REPLACE FUNCTION schema_support.rebuild_stamp_triggers()
+-------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION schema_support.rebuild_stamp_triggers
+    (tbl_schema VARCHAR)
 RETURNS VOID AS $$
 BEGIN
-	DECLARE
-		tab	RECORD;
-	BEGIN
-		FOR tab IN 
-			SELECT 
-				table_name
-			FROM
-				information_schema.tables
-			WHERE
-				table_schema = 'jazzhands' AND
-				table_type = 'BASE TABLE' AND
-				table_name NOT LIKE 'aud$%'
-		LOOP
-			PERFORM schema_support.rebuild_stamp_trigger(tab.table_name);
-		END LOOP;
-	END;
+    DECLARE
+        tab RECORD;
+    BEGIN
+        FOR tab IN 
+            SELECT table_name FROM information_schema.tables
+            WHERE table_schema = tbl_schema AND table_type = 'BASE TABLE'
+            AND table_name NOT LIKE 'aud$%'
+        LOOP
+            PERFORM schema_support.rebuild_stamp_trigger
+	        (tbl_schema, tab.table_name);
+        END LOOP;
+    END;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
