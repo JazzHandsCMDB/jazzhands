9003602a1703bff71486989eaf04217c41d99e2d
Todd Kover <kovert@omnisceint.com>
2013-10-04 13:22:02 -0400

    add cycle detection and some array sorting to v_netblock_hier (recursive). Backwards compatible

--- database/ddl/views/pgsql/create_v_netblock_hier.sql
+++ database/ddl/views/pgsql/create_v_netblock_hier.sql
@@ -50,7 +50,10 @@ WITH RECURSIVE var_recurse (
 	IS_IPV4_ADDRESS,
 	description,
 	parent_netblock_id,
-	site_code
+	site_code,
+	array_path,
+	array_ip_path,
+	cycle
 ) as  (
 	select  0			as netblock_level,
 		nb.netblock_id		as root_netblock_id,
@@ -63,7 +66,10 @@ WITH RECURSIVE var_recurse (
 		nb.IS_IPV4_ADDRESS,
 		nb.description,
 		nb.parent_netblock_id,
-		snb.site_code
+		snb.site_code,
+		ARRAY[nb.netblock_id] as "array",
+		ARRAY[nb.ip_address] as "array",
+		false as bool
 	  from  netblock nb
 		left join site_netblock snb
 			on snb.netblock_id = nb.netblock_id
@@ -80,12 +86,32 @@ UNION ALL
 		nb.IS_IPV4_ADDRESS,
 		nb.description,
 		nb.parent_netblock_id,
-		snb.site_code
+		snb.site_code,
+		x.array_path || nb.netblock_id AS array_path,
+		x.array_ip_path || nb.ip_address AS array_ip_path,
+		nb.netblock_id = ANY (x.array_path)
 	  from  var_recurse x
 	  	inner join netblock nb
 			on x.netblock_id = nb.parent_netblock_id
 		left join site_netblock snb
 			on snb.netblock_id = nb.netblock_id
 	where   nb.IS_SINGLE_ADDRESS = 'N'
-) SELECT * from var_recurse;
+	and	NOT cycle
+) SELECT 
+	netblock_level,
+	root_netblock_id,
+	ip,
+	netblock_id,
+	ip_address,
+	netmask_bits,
+	netblock_status,
+	IS_SINGLE_ADDRESS,
+	IS_IPV4_ADDRESS,
+	description,
+	parent_netblock_id,
+	site_code,
+	array_to_string(var_recurse.array_path, '/'::text) AS text_path,
+	array_path,
+	array_ip_path
+from var_recurse;
 ;
