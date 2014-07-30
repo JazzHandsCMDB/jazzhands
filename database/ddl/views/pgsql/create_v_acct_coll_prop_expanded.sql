94e2bb44939ec9fc0bfa39fb199cf9b8fdf9c091
Todd Kover <kovert@omniscient.com>
2014-07-30 13:28:11 -0400

    - add missing copyrights - adjust v_acct_coll_prop_expanded so that it does not order by but rather   passes the order ranking up as a column so the end user can order by

--- database/ddl/views/pgsql/create_v_acct_coll_prop_expanded.sql
+++ database/ddl/views/pgsql/create_v_acct_coll_prop_expanded.sql
@@ -1,3 +1,19 @@
+-- Copyright (c) 2012-2013, Todd M. Kover
+-- Copyright (c) 2014, Todd M. Kover
+-- All rights reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 CREATE OR REPLACE VIEW v_acct_coll_prop_expanded AS
 	SELECT
 		root_account_collection_id as account_collection_id,
@@ -14,29 +30,30 @@ CREATE OR REPLACE VIEW v_acct_coll_prop_expanded AS
 		property_value_person_id,
 		property_value_sw_package_id,
 		property_value_token_col_id,
+		property_rank,
 		CASE is_multivalue WHEN 'N' THEN false WHEN 'Y' THEN true END 
-			is_multivalue
+			is_multivalue,
+		CASE account_collection_type
+			WHEN 'per-user' THEN 0
+			ELSE CASE assign_method
+				WHEN 'DirectAccountCollectionAssignment' THEN 10
+				WHEN 'DirectDepartmentAssignment' THEN 200
+				WHEN 'DepartmentAssignedToAccountCollection' THEN 300
+						+ dept_level + acct_coll_level
+				WHEN 'AccountAssignedToChildDepartment' THEN 400
+						+ dept_level 
+				WHEN 'AccountAssignedToChildAccountCollection' THEN 500
+						+ acct_coll_level
+				WHEN 'DepartmentAssignedToChildAccountCollection' THEN 600
+						+ dept_level + acct_coll_level
+				WHEN 'ChildDepartmentAssignedToAccountCollection' THEN 700
+						+ dept_level + acct_coll_level
+				WHEN 'ChildDepartmentAssignedToChildAccountCollection' THEN 800
+						+ dept_level + acct_coll_level
+				ELSE 999
+			END END as assign_rank
 	FROM
 		v_acct_coll_expanded_detail JOIN
 		account_collection ac USING (account_collection_id) JOIN
 		v_property USING (account_collection_id) JOIN
-		val_property USING (property_name, property_type)
-	ORDER BY
-		CASE account_collection_type
-			WHEN 'per-user' THEN 0
-			ELSE 99
-			END,
-		CASE assign_method
-			WHEN 'DirectAccountCollectionAssignment' THEN 0
-			WHEN 'DirectDepartmentAssignment' THEN 1
-			WHEN 'DepartmentAssignedToAccountCollection' THEN 2
-			WHEN 'AccountAssignedToChildDepartment' THEN 3
-			WHEN 'AccountAssignedToChildAccountCollection' THEN 4
-			WHEN 'DepartmentAssignedToChildAccountCollection' THEN 5
-			WHEN 'ChildDepartmentAssignedToAccountCollection' THEN 6
-			WHEN 'ChildDepartmentAssignedToChildAccountCollection' THEN 7
-			ELSE 99
-			END,
-		dept_level,
-		acct_coll_level,
-		account_collection_id;
+		val_property USING (property_name, property_type);
