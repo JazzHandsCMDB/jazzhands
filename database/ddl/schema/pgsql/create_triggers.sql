4ee94c8f03163f6eb7e1a7b3543f19424f710b44
Todd Kover <kovert@omniscient.com>
2014-09-10 18:12:02 -0400

    add device, svcenv and token collection loop detection (basic)

--- database/ddl/schema/pgsql/create_triggers.sql
+++ database/ddl/schema/pgsql/create_triggers.sql
@@ -1105,6 +1105,76 @@ CREATE TRIGGER trigger_check_netblock_collection_hier_loop
 AFTER INSERT OR UPDATE ON netblock_collection_hier
 	FOR EACH ROW EXECUTE PROCEDURE check_netblock_colllection_hier_loop();
 
+/*
+ * Do not let hierarchy point to itself.  This shoudl probably be extended
+ * to check up/down the hierarchy to prevent loops.  Needs to be ported to
+ * oracle XXX
+ */
+CREATE OR REPLACE FUNCTION check_device_colllection_hier_loop()
+	RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.device_collection_id = NEW.parent_device_collection_id THEN
+		RAISE EXCEPTION 'device Collection Loops Not Pernitted '
+			USING ERRCODE = 20704;	/* XXX */
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+DROP TRIGGER IF EXISTS trigger_check_device_collection_hier_loop 
+	ON device_collection_hier;
+CREATE TRIGGER trigger_check_device_collection_hier_loop 
+AFTER INSERT OR UPDATE ON device_collection_hier
+	FOR EACH ROW EXECUTE PROCEDURE check_device_colllection_hier_loop();
+
+/*
+ * Do not let hierarchy point to itself.  This shoudl probably be extended
+ * to check up/down the hierarchy to prevent loops.  Needs to be ported to
+ * oracle XXX
+ */
+CREATE OR REPLACE FUNCTION check_token_colllection_hier_loop()
+	RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.token_collection_id = NEW.child_token_collection_id THEN
+		RAISE EXCEPTION 'token Collection Loops Not Pernitted '
+			USING ERRCODE = 20704;	/* XXX */
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+DROP TRIGGER IF EXISTS trigger_check_token_collection_hier_loop 
+	ON token_collection_hier;
+CREATE TRIGGER trigger_check_token_collection_hier_loop 
+AFTER INSERT OR UPDATE ON token_collection_hier
+	FOR EACH ROW EXECUTE PROCEDURE check_token_colllection_hier_loop();
+
+
+/*
+ * Do not let hierarchy point to itself.  This shoudl probably be extended
+ * to check up/down the hierarchy to prevent loops.  Needs to be ported to
+ * oracle XXX
+ */
+CREATE OR REPLACE FUNCTION check_svcenv_colllection_hier_loop()
+	RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.service_env_collection_idw = 
+		NEW.child_service_env_coll_id THEN
+			RAISE EXCEPTION 'svcenv Collection Loops Not Pernitted '
+			USING ERRCODE = 20704;	/* XXX */
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql SECURITY DEFINER;
+
+DROP TRIGGER IF EXISTS trigger_check_svcenv_collection_hier_loop 
+	ON svcenv_collection_hier;
+CREATE TRIGGER trigger_check_svcenv_collection_hier_loop 
+AFTER INSERT OR UPDATE ON service_environment_coll_hier
+	FOR EACH ROW EXECUTE PROCEDURE check_svcenv_colllection_hier_loop();
+
+
+
 
 /*
  * enforces is_multivalue in val_person_image_usage
