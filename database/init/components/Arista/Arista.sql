248d7351b2d26b83758496b72a0ad0ca77364fba
Matthew Ragan <mdr@appnexus.com>
2015-05-13 14:48:01 -0400

    Move Arista components around and add support for the 7050X

--- database/init/components/Arista/Arista.sql
+++ database/init/components/Arista/Arista.sql
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-\ir Arista_7050ST-FR.sql
+\ir Arista_7050.sql
+\ir Arista_7050X.sql
 \ir Arista_7050Q-FR.sql
 \ir Arista_7500.sql
