6b217c647932c55f998683c81a4b58f21f619efe
Todd Kover <kovert@omniscient.com>
2015-08-13 17:39:56 +0000

    fixes

--- database/attest/process-jira.pl
+++ database/attest/process-jira.pl
@@ -184,7 +184,6 @@ sub open_new_issues {
 				a.account_id = ais.approver_account_id
 	        Where     approval_type = 'jira-hr'
 	        AND     ais.is_completed = 'N'
-			AND		aii.is_approved IS NULL
 			AND		ais.external_reference_name IS NULL
 	        ORDER BY approval_instance_step_id, approved_lhs, approved_label
 	}
@@ -258,6 +257,7 @@ sub check_pending_issues {
 				a.account_id = ais.approver_account_id
 	        Where     approval_type = 'jira-hr'
 	        AND     ais.is_completed = 'N'
+			AND		aii.is_approved IS NULL
 			AND		ais.external_reference_name IS NOT NULL
 	        ORDER BY approval_instance_step_id, approved_lhs, approved_label
 	}
