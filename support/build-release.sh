794b9c900839062218b04526b5560f9d5f30bf82
Todd Kover <kovert@appnexus.com>
2015-12-24 20:02:16 +0000

    first pass at making jazzhands-perl-common a deb

--- support/build-release.sh
+++ support/build-release.sh
@@ -11,14 +11,22 @@ mkdir $RELEASEDIR
 for input in `cat releases | sed 's/#.*$//'` ; do
 	dirname=`dirname $input`
 	basename=`basename $input | sed s/.spec//`
-	if [ `expr $input : '.*\.spec'` -eq 0 ] ; then
-		echo 1>&2 $input is not a spec based release.  Unsupported.  DIE.
-		exit 1
-		rootname=$dirname
+	if [ -x /usr/bin/rpm ] ; then
+		if [ `expr $input : '.*\.spec'` -eq 0 ] ; then
+			echo 1>&2 $input is not a spec based release.  Unsupported.  DIE.
+			exit 1
+			rootname=$dirname
+		else
+			spec="$input"
+			echo Processing $spec ...
+			rootname=`rpm -q --specfile $ROOT/$spec --queryformat '%{name}-%{version}\n' | head -1`
+		fi
+	elif [ -x /usr/bin/dpkg ] ; then
+		NAME=`(cd $ROOT/$dirname ; dpkg-parsechangelog | grep ^Source | head | awk '{print $NF}')`
+		VERSION=`(cd $ROOT/$dirname ; dpkg-parsechangelog | grep ^Version | head | awk '{print $NF}')`
+		rootname=${NAME}-${VERSION}
 	else
-		spec="$input"
-		echo Processing $spec ...
-		rootname=`rpm -q --specfile $ROOT/$spec --queryformat '%{name}-%{version}\n' | head -1`
+		echo 1>&2 $input 'can not be built on this host (rpm/dpkg required)'
 	fi
 	mkdir $RELEASEDIR/$rootname
 	(cd $ROOT/$dirname ; tar cf - . ) | (cd $RELEASEDIR/$rootname ; tar xpf  - )
