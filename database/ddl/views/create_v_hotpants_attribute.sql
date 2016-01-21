edea1a54c8d5fc2ff6b1922b8ae2c287c7939399
Todd Kover <kovert@omniscient.com>
2016-01-21 17:06:37 -0500

    more optimizations

--- database/ddl/views/create_v_hotpants_attribute.sql
+++ database/ddl/views/create_v_hotpants_attribute.sql
@@ -22,13 +22,27 @@ SELECT	login,
 	property_type,
 	property_value,
 	is_boolean,
-	device_collection_id
+	device_collection_id,
+	'account' as attribute_style
 FROM	v_dev_col_user_prop_expanded 
 	INNER JOIN Device_Collection USING (Device_Collection_ID)
 WHERE	is_enabled = 'Y'
 AND	(
-		Device_Collection_Type = 'HOTPants-app'
+		Device_Collection_Type IN ('HOTPants-app', 'HOTPants')
 	OR
 		Property_Type IN ('RADIUS', 'HOTPants') 
 	)
+UNION
+SELECT  NULL			as login,
+	property_name,
+	property_type,
+	Property_Value_Password_Type	as property_value,
+	0				as is_boolean,
+	device_collection_id,
+	'device'
+        FROM    property
+        WHERE   Property_Name = 'PWType'
+        AND     Property_Type = 'HOTPants'
+        AND     account_collection_id IS NULL
+
 ;
