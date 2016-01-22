78f43ce16e943f3dae1d5752e505ea8759b54adf
Todd Kover <kovert@omniscient.com>
2016-01-22 13:23:46 -0500

    more fixes to make bits build from scratch

--- database/init/initialize_jazzhands.sql
+++ database/init/initialize_jazzhands.sql
@@ -476,7 +476,7 @@ insert into val_person_company_attr_dtype (person_company_attr_data_type) values
 	('person_id');
 
 insert into val_property_type (property_type, description,is_multivalue) 
-	values 
+	VALUES 
 	('TokenMgmt', 'Allow administrators to manage OTP tokens', 'Y'),
 	('UserMgmt', 'Allow administrators to manage users', 'Y'),
 	('feed-attributes','configurable attributes on user feeds', 'Y'),
@@ -512,7 +512,7 @@ INSERT INTO val_property (
 	property_name, property_type, property_data_type,
 	description,
 	permit_account_collection_id, permit_device_collection_id
-VALUES (
+) VALUES (
 	'PWType', 'HOTPants', 'password_type',
 	'Set password verification type for this Device and maybe account collection',
 	'ALLOWED',     'REQUIRED'
@@ -1607,7 +1607,8 @@ VALUES
         ('destored'),
         ('stolen');
 
-INSERT INTO val_token_type (token_type, description, token_digit_count)
+INSERT INTO val_token_type 
+	(token_type, description, token_digit_count)
 VALUES
         ('soft_seq', 'sequence based soft token', 6),
         ('soft_time', 'time-based soft token', 6);
