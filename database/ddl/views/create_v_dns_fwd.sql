25154143aedd612b114c0403a493b6cfa45c3066
Todd Kover <kovert@omniscient.com>
2016-07-08 17:27:36 -0400

    bug fixes

--- database/ddl/views/create_v_dns_fwd.sql
+++ database/ddl/views/create_v_dns_fwd.sql
@@ -20,9 +20,9 @@
 
 CREATE OR REPLACE VIEW v_dns_fwd AS
 SELECT * FROM  (
-       SELECT  
-	    d.dns_record_id, 
-		NULL as network_range_id,
+       SELECT
+	    d.dns_record_id,
+		NULL::integer as network_range_id,
 		d.dns_domain_id,
 		d.dns_name, d.dns_ttl, d.dns_class,
 	    d.dns_type, d.dns_value,
@@ -54,11 +54,11 @@ SELECT * FROM  (
 			using (netblock_id)
 	    ) dv on d.dns_value_record_id = dv.dns_record_id
 	UNION
-       SELECT  
+       SELECT
 			NULL as dns_record_id,
 			network_range_id,
 			dns_domain_id,
-			concat(coalesce(dns_prefix, 'pool'), '-', 
+			concat(coalesce(dns_prefix, 'pool'), '-',
 				replace(host(ip)::text, '.', '-')) as dns_name,
 			NULL as dns_ttl, 'IN' as dns_class,
 			CASE WHEN family(ip::inet) = 4 THEN 'A' ELSE 'AAAA' END as dns_type,
@@ -77,7 +77,7 @@ SELECT * FROM  (
 			NULL as val_value,
 	    NULL as val_ip
 	FROM (
-       select  
+       select
 		network_range_id,
 	    	dns_domain_id,
 	    	dns_prefix,
@@ -95,7 +95,7 @@ WHERE  dns_type != 'REVERSE_ZONE_BLOCK_PTR'
 	UNION
 	SELECT
 		dns_record_id,
-		NULL AS network_range_id,
+		NULL::integer AS network_range_id,
 		parent_dns_domain_id as dns_domain_id,
 		regexp_replace(soa_name, '\.' || pdom.parent_soa_name || '$', '') as dns_name,
 		dns_ttl,
@@ -103,22 +103,23 @@ WHERE  dns_type != 'REVERSE_ZONE_BLOCK_PTR'
 		dns_type,
 		dns_value,
 		dns_priority,
-		NULL AS ip,
-		NULL AS ref_record_id,
-		NULL AS ref_dns_name,
-		NULL AS dns_srv_service,
-		NULL AS dns_srv_protocol,
-		NULL AS dns_srv_weight,
-		NULL AS dns_srv_port,
+		NULL::inet AS ip,
+		NULL::integer AS ref_record_id,
+		NULL::text AS ref_dns_name,
+		NULL::text AS dns_srv_service,
+		NULL::text AS dns_srv_protocol,
+		NULL::integer AS dns_srv_weight,
+		NULL::integer AS dns_srv_port,
 		is_enabled AS is_enabled,
 		NULL AS val_dns_name,
 		NULL AS val_domain,
 		NULL AS val_value,
-		NULL AS val_ip
+		NULL::inet AS val_ip
 	FROM	dns_record join dns_domain using (dns_domain_id)
 		join (select dns_domain_id as parent_dns_domain_id,
 			soa_name as parent_soa_name from dns_domain)  pdom
 			USING(parent_dns_domain_id)
 	WHERE	dns_class = 'IN' AND dns_type = 'NS'
+	AND dns_name IS NULL
 	AND	parent_dns_domain_id is not NULL
 ;
