7d0d8acfc3fbcbebf2fe4f1d952ec99c2220946b
Todd Kover <kovert@omniscient.com>
2016-09-14 10:40:16 -0400

    add dns views

--- database/ddl/views/create_extra_views_pgsql.sql
+++ database/ddl/views/create_extra_views_pgsql.sql
@@ -145,5 +145,9 @@
 \ir create_v_person.sql
 \ir create_v_person_company.sql
 
+\ir create_v_dns_rvs.sql
+\ir create_v_dns_fwd.sql
+\ir create_v_dns.sql
+
 -- too be retired
 \ir create_x509_certificate.sql
