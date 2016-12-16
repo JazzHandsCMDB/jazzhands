5423aaecea81f52c79efe33b553dcf2af13924de
Todd Kover <kovert@omniscient.com>
2016-12-16 17:19:41 -0500

    add account collection relation triggers and tests

--- database/ddl/schema/pgsql/create_account_coll_relation_triggers.sql
+++ database/ddl/schema/pgsql/create_account_coll_relation_triggers.sql
@@ -93,6 +93,52 @@ CREATE TRIGGER trigger_acct_coll_remove_direct
 
 
 ----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION acct_coll_update_direct_before()
+RETURNS TRIGGER AS $$
+BEGIN
+	SET CONSTRAINTS fk_acct_coll_rel_type_type DEFERRED;
+END;
+$$
+SET search_path=jazzhands
+LANGUAGE plpgsql SECURITY DEFINER;
+
+DROP TRIGGER IF EXISTS trigger_acct_coll_update_direct_before
+	 ON val_account_collection_type;
+CREATE TRIGGER trigger_acct_coll_update_direct_before
+        BEFORE UPDATE OF account_collection_type
+        ON val_account_collection_type
+        FOR EACH ROW
+        EXECUTE PROCEDURE acct_coll_update_direct_before();
+
+
+----------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION acct_coll_update_direct_before()
+RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE account_coll_type_relation
+	SET account_collection_type = NEW.account_collection_type
+	WHERE account_collection_type = OLD.account_collection_type;
+
+	SET CONSTRAINTS fk_acct_coll_rel_type_type IMMEDIATE;
+END;
+$$
+SET search_path=jazzhands
+LANGUAGE plpgsql SECURITY DEFINER;
+
+DROP TRIGGER IF EXISTS trigger_acct_coll_update_direct_before
+	 ON val_account_collection_type;
+CREATE TRIGGER trigger_acct_coll_update_direct_before
+        AFTER UPDATE OF account_collection_type
+        ON val_account_collection_type
+        FOR EACH ROW
+        EXECUTE PROCEDURE acct_coll_update_direct_before();
+
+
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
 
 
 CREATE OR REPLACE FUNCTION account_coll_member_relation_enforce()
@@ -112,8 +158,9 @@ BEGIN
 	IF act.MAX_NUM_MEMBERS IS NOT NULL THEN
 		SELECT count(*)
 		  INTO tally
-		  FROm account_collection_account
-		  WHERE account_collection_id = NEW.account_collection_id
+		  FROM account_collection_account
+		  		JOIN account_collection USING (account_collection_id)
+		  WHERE account_collection_type = act.account_collection_type
 		  AND account_collection_relation = NEW.account_collection_relation;
 
 		IF tally > act.MAX_NUM_MEMBERS THEN
