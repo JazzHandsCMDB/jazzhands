54e5511f20b88e2020667118c4e5ce1b1ac5ce03
Todd Kover <kovert@omniscient.com>
2017-08-07 20:11:03 -0400

    adjust v_dns_changes_pending to handle ip universes better.  There is still some work needed, I suspect.

--- database/ddl/views/create_v_dns_changes_pending.sql
+++ database/ddl/views/create_v_dns_changes_pending.sql
@@ -22,7 +22,19 @@
 --
 
 --
--- XXX - This likely needs to be completely overhauled.
+-- XXX - This likely needs more work to get all the ip universe boundary
+-- stuff better.
+--
+--
+-- The WITH chg query takes all the cidr blocks in dns_change_record
+-- and makes them into dns records.    NOTE:  They do not properly handle
+-- things that are not on classful boundaries, which really needs to be fixed.
+--
+-- The first one does in-addr networks pulled from the changes.  Its probably
+--	more agressive than it should be
+-- The second one does zones without ip universes (does all)
+-- The third does explicitly set ip universes and only those
+-- The fourth acts like the third but expands ones visible to the one set
 --
 CREATE OR REPLACE VIEW v_dns_changes_pending AS
 WITH chg AS (
@@ -32,22 +44,43 @@ WITH chg AS (
 		dns_utils.get_domain_from_cidr(ip_address) as cidrdns
 	FROM dns_change_record
 	WHERE ip_address is not null
-) SELECT DISTINCT *
+) SELECT *
 FROM (
-	SELECT	chg.dns_change_record_id, n.dns_domain_id,
+	SELECT	chg.dns_change_record_id, n.dns_domain_id, du.ip_universe_id,
 		du.should_generate, du.last_generated,
 		n.soa_name, chg.ip_address
 	FROM   chg
 		INNER JOIN dns_domain n on chg.cidrdns = n.soa_name
 		INNER JOIN dns_domain_ip_universe du ON
 			du.dns_domain_id = n.dns_domain_id
-	UNION ALL
-	SELECT  chg.dns_change_record_id, d.dns_domain_id,
+UNION ALL
+	SELECT  chg.dns_change_record_id, d.dns_domain_id, du.ip_universe_id,
 		du.should_generate, du.last_generated,
 		d.soa_name, NULL
 	FROM	dns_change_record chg
 		INNER JOIN dns_domain d USING (dns_domain_id)
 		INNER JOIN dns_domain_ip_universe du USING (dns_domain_id)
 	WHERE   dns_domain_id IS NOT NULL
+	AND chg.ip_universe_id IS NULL
+UNION ALL
+	SELECT  chg.dns_change_record_id, d.dns_domain_id, ip_universe_id,
+		du.should_generate, du.last_generated,
+		d.soa_name, NULL
+	FROM	dns_change_record chg
+		INNER JOIN dns_domain d USING (dns_domain_id)
+		INNER JOIN dns_domain_ip_universe du USING (dns_domain_id,ip_universe_id)
+	WHERE   dns_domain_id IS NOT NULL
+	AND chg.ip_universe_id IS NOT NULL
+UNION ALL
+	SELECT  chg.dns_change_record_id, d.dns_domain_id, 
+		iv.visible_ip_universe_id,
+		du.should_generate, du.last_generated,
+		d.soa_name, NULL
+	FROM	dns_change_record chg
+		INNER JOIN ip_universe_visibility iv USING (ip_universe_id)
+		INNER JOIN dns_domain d USING (dns_domain_id)
+		INNER JOIN dns_domain_ip_universe du USING (dns_domain_id)
+	WHERE   dns_domain_id IS NOT NULL
+	AND chg.ip_universe_id IS NOT NULL
 ) x
 ;
