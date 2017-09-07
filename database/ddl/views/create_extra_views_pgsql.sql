b86a1c76cd6783b239bdf78cd2a947029fea05d6
Todd Kover <kovert@omniscient.com>
2017-09-07 14:48:39 -0400

    reorder views

--- database/ddl/views/create_extra_views_pgsql.sql
+++ database/ddl/views/create_extra_views_pgsql.sql
@@ -106,6 +106,8 @@
 \ir create_v_corp_family_account.sql
 
 \ir pgsql/create_v_person_company_hier.sql
+\ir create_v_person.sql
+\ir create_v_person_company.sql
 
 -- possibly to replace v_device_col_acct_col_expanded
 \ir create_v_device_col_acct_col_unixlogin.sql
@@ -152,9 +154,6 @@
 \ir pgsql/create_v_l2_network_coll_expanded.sql
 \ir pgsql/create_v_l3_network_coll_expanded.sql
 \ir pgsql/create_v_layer3_network_expanded.sql
-
-\ir create_v_person.sql
-\ir create_v_person_company.sql
 
 \ir create_v_dns_fwd.sql
 \ir create_v_dns_rvs.sql
