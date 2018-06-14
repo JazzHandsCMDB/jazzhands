e9046b9af4e493c2023b9c1eece9194e76f68415
Matthew Ragan <mdr@sucksless.net>
2018-06-14 11:24:37 -0400

    Overhauled v_device_components_json because of things

--- database/ddl/views/pgsql/create_v_device_components_json.sql
+++ database/ddl/views/pgsql/create_v_device_components_json.sql
@@ -23,40 +23,112 @@ WITH ctf AS (
 		jazzhands.component_type_component_func ctcf
 	GROUP BY
 		ctcf.component_type_id
-), cs AS (
-	SELECT
-		cp.component_type_id,
-		cp.property_value::bigint AS size
-	FROM
-		jazzhands.component_property cp
-	WHERE
-		(cp.component_property_name,cp.component_property_type) IN
-			(('DiskSize', 'disk'),
-			 ('MemorySize', 'memory'))
-), comp_json AS (
+), cpu_info AS (
 	SELECT
 		c.component_id,
 		jsonb_build_object(
 			'component_id', c.component_id,
+			'component_type_id', c.component_type_id,
+			'company_name', comp.company_name,
+			'model', ct.model,
+			'core_count', pc.property_value::bigint,
+			'processor_speed', ps.property_value
+		) as component_json
+	FROM
+		component c
+		JOIN component_type ct USING (component_type_id)
+		JOIN component_type_component_func ctcf USING (component_type_id)
+		JOIN component_property pc ON (
+			ct.component_type_id = pc.component_type_id
+			AND (pc.component_property_name, pc.component_property_type) = 
+				('ProcessorCores', 'CPU')
+		)
+		JOIN component_property ps ON (
+			ct.component_type_id = ps.component_type_id
+			AND (ps.component_property_name, ps.component_property_type) = 
+				('ProcessorSpeed', 'CPU')
+		)
+		LEFT JOIN company comp USING (company_id)
+	WHERE
+		ctcf.component_function = 'CPU'
+), disk_info AS (
+	SELECT
+		c.component_id,
+		jsonb_build_object(
+			'component_id', c.component_id,
+			'component_type_id', c.component_type_id,
+			'company_name', comp.company_name,
 			'model', ct.model,
 			'serial_number', a.serial_number,
-			'functions', ctf.functions,
-			'slot_name', s.slot_name,
-			'size', cs.size
-		) as json_component
+			'size_bytes', ds.property_value::bigint,
+			'size', CEIL(ds.property_value::bigint / 1073741824::numeric) ||
+				'G'::text,
+			'protocol', dp.property_value,
+			'media_type', mt.property_value
+		) as component_json
 	FROM
-		jazzhands.component c LEFT JOIN
-		jazzhands.component_type ct USING (component_type_id) JOIN
-		ctf USING (component_type_id) JOIN
-		jazzhands.asset a ON c.component_id = a.component_id LEFT JOIN
-		cs USING (component_type_id) LEFT JOIN
-		jazzhands.slot s ON c.parent_slot_id = s.slot_id
+		component c
+		JOIN component_type ct USING (component_type_id)
+		JOIN component_type_component_func ctcf USING (component_type_id)
+		LEFT JOIN asset a USING (component_id)
+		JOIN component_property ds ON (
+			ct.component_type_id = ds.component_type_id
+			AND (ds.component_property_name, ds.component_property_type) = 
+				('DiskSize', 'disk')
+		)
+		JOIN component_property dp ON (
+			ct.component_type_id = dp.component_type_id
+			AND (dp.component_property_name, dp.component_property_type) = 
+				('DiskProtocol', 'disk')
+		)
+		JOIN component_property mt ON (
+			ct.component_type_id = mt.component_type_id
+			AND (mt.component_property_name, mt.component_property_type) = 
+				('MediaType', 'disk')
+		)
+		LEFT JOIN company comp USING (company_id)
+	WHERE
+		ctcf.component_function = 'disk'
+), memory_info AS (
+	SELECT
+		c.component_id,
+		jsonb_build_object(
+			'component_id', c.component_id,
+			'component_type_id', c.component_type_id,
+			'company_name', comp.company_name,
+			'model', ct.model,
+			'serial_number', a.serial_number,
+			'size', msize.property_value::bigint,
+			'speed', mspeed.property_value
+		) as component_json
+	FROM
+		component c
+		JOIN component_type ct USING (component_type_id)
+		JOIN component_type_component_func ctcf USING (component_type_id)
+		LEFT JOIN asset a USING (component_id)
+		JOIN component_property msize ON (
+			ct.component_type_id = msize.component_type_id
+			AND (msize.component_property_name, msize.component_property_type) = 
+				('MemorySize', 'memory')
+		)
+		JOIN component_property mspeed ON (
+			ct.component_type_id = mspeed.component_type_id
+			AND (mspeed.component_property_name, mspeed.component_property_type) = 
+				('MemorySpeed', 'memory')
+		)
+		LEFT JOIN company comp USING (company_id)
+	WHERE
+		ctcf.component_function = 'memory'
 )
 SELECT
 	dc.device_id,
-	jsonb_agg(comp_json.json_component) as device_components
+	jsonb_agg(x.component_json)
 FROM
 	jazzhands.v_device_components dc JOIN
-	comp_json USING (component_id) 
+	(
+		SELECT * FROM cpu_info UNION
+		SELECT * FROM disk_info UNION
+		SELECT * FROM memory_info
+	) x USING (component_id)
 GROUP BY
-	device_id;
+	dc.device_id;
