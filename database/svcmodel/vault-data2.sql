d0408f4e0e8402036e46ceb8c54e197012ea7ab1
Todd M. Kover <kovert@xandr.com>
2020-06-18 16:02:23 +0000

    add more vault data to run later

---
+++ database/svcmodel/vault-data2.sql
@@ -0,0 +1,57 @@
+WITH v AS (
+	INSERT INTO vault_policy (
+		vault_policy_name
+	) VALUES (
+		'tang-a-production-consumer'
+	) RETURNING *
+), first AS (
+	INSERT INTO vault_policy_path (
+		vault_policy_id, read, vault_policy_path
+	) SELECT vault_policy_id, true,
+		'global/kv/data/services/tang/environments/production/a/*'
+	FROM v
+	RETURNING *
+), second AS (
+	INSERT INTO vault_policy_path (
+		vault_policy_id, list, vault_policy_path
+	) SELECT vault_policy_id, true,
+		'global/kv/metadata/services/tang/environments/production/a/*'
+	FROM v
+	RETURNING *
+) SELECT * FROM first UNION SELECT * FROM second;
+
+WITH v AS (
+	INSERT INTO vault_policy (
+		vault_policy_name
+	) VALUES (
+		'drivescale-development-admin-consumer'
+	) RETURNING *
+), first AS (
+	INSERT INTO vault_policy_path (
+		vault_policy_id, read, vault_policy_path
+	) SELECT vault_policy_id, true,
+		'global/kv/data/services/drivescale/environments/development/admin/*'
+	FROM v
+	RETURNING *
+), second AS (
+	INSERT INTO vault_policy_path (
+		vault_policy_id, list, vault_policy_path
+	) SELECT vault_policy_id, true,
+		'global/kv/metadata/services/drivescale/environments/development/admin/*'
+	FROM v
+	RETURNING *
+) SELECT * FROM first UNION SELECT * FROM second;
+
+
+--- === === === ===
+
+\set ECHO queries
+
+SELECT * FROM vault_policy ORDER BY 1;
+SELECT * FROM vault_policy_path ORDER BY 1;
+
+SELECT * FROM vault_policy_mclass;
+
+-- XXX need to incrporate user and group for mclass
+
+\set ECHO none
