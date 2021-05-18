9bba7a2013af6b0663d359a959753355411530bd
Todd Kover <kovert@omniscient.com>
2021-05-18 15:55:06 -0400

    Fix issue with netblock_utils.find_best_parent_id not passing arguments through to the new function.

--- database/pkg/pgsql/netblock_utils.sql
+++ database/pkg/pgsql/netblock_utils.sql
@@ -93,7 +93,8 @@ BEGIN
 		ip_universe_id		:= in_ip_universe_id,
 		is_single_address	:= p_single,
 		netblock_id			:= in_netblock_id,
-		fuzzy_can_subnet	:= in_fuzzy_can_subnet
+		fuzzy_can_subnet	:= in_fuzzy_can_subnet,
+		can_fix_can_subnet	:= can_fix_can_subnet
 	);
 END;
 $$
