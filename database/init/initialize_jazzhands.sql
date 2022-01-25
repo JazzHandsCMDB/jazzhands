c12ee3a14b60dbd07001e4d5ce251e9f9d91035c
Bernard Jech <bjech@appnexus.com>
2022-01-25 15:55:59 +0100

    populate val_x509_fingerprint_hash_algorithm in init/initialize_jazzhands.sql

--- database/init/initialize_jazzhands.sql
+++ database/init/initialize_jazzhands.sql
@@ -1799,6 +1799,12 @@ insert into val_encryption_key_purpose
 values
 	('certpassphrase', 1, 'SSL Crtificates Key Passphrase');
 
+insert into val_x509_fingerprint_hash_algorithm
+	(x509_fingerprint_hash_algorighm, description)
+values
+	('sha1', 'SHA1 hash'),
+	('sha256', 'SHA256 hash');
+
 insert into val_x509_certificate_file_format
 	(x509_certificate_file_format, description)
 values
