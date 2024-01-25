6e2114728ce77a5917ccbcb0d919b7037533428e
gmtime <nenad@buncic.net>
2024-01-25 14:26:53 +0100

    Adding retry logic to dhcpgen

--- management/dhcpgen/dhcpgen.pl
+++ management/dhcpgen/dhcpgen.pl
@@ -331,12 +331,23 @@ sub do_rebuild {
 	my $log = $opt->{'log'};
 
 	my $jh;
-	my $ret;
 
-	$ret = generate_dhcp_configs(
-		conf => $conf,
-		errors => \@errors
-	);
+	my $ret   = 1;
+	my $retry = 5;
+
+	while( $retry ) {
+		$ret = generate_dhcp_configs(
+			conf => $conf,
+			errors => \@errors
+		);
+		$retry = 0 if( $ret eq 1 );
+		if( $retry ) {
+			$log->warn(sprintf("WARN: %s", join ("\n", @errors)));
+			$log->warn( "WARN: Retrying..." );
+			$retry -= 1;
+			sleep( 60 );
+		}
+	}
 
 	if ($ret) {
 		$log->info ("Rebuild completed successfully");
@@ -1141,7 +1152,7 @@ host %s-%d-%d-%d {
 	close $masterfh;
 
 	if (defined(&_LocalHooks::postrun)) {
-		_LocalHooks::postrun(
+		return _LocalHooks::postrun(
 			dbh => $jh,
 			conf => $conf,
 			local_options => $local_options,
